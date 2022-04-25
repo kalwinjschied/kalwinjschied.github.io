2b7b02e70141116be174d9519f45edf2c6f18a0b
kalwinjschied <78454469+kalwinjschied@users.noreply.github.com>
2022-04-25 11:40:11 -1100

    Add files via upload

---
+++ 01Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: 
+    Kalwin Schied 
+    File Name: script.js
+    Date: 
+*/
+
+//jQueery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
