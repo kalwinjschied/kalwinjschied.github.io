2d8a106b848e0c24bf96ac8e7868eebca5172e17
kalwinjschied <78454469+kalwinjschied@users.noreply.github.com>
2022-12-08 15:48:17 -0700

    expirement

---
+++ component3.vue
@@ -0,0 +1,26 @@
+<script>
+import BlogPost from './BlogPost.vue'
+  
+export default {
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+	<BlogPost 
+    v-for="post in posts"
+	  :key="post.id"
+  	:title="post.title"
+	></BlogPost>
+</template>
